8c4d317d50dd285de3b2a178341c108f62eec38d
Luis Guillen <luisguillenortiz_8@hotmail.com>
2023-07-23 20:18:47 -0600

    levantando servidor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const sql = require('mssql');
-
+const cors=require('cors');
+const bodyParser= require('body-parser');
+const puerto=3001;
 
 
 const app = express();
 
+//mildrewers
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
+app.get('/', (req, res) => {
+    res.send('Probando backend de Airbnb');
+});
 
-console.log('hola')
+app.listen(puerto, () => {
+	console.log(`Servidor Levantado en https://localhost:${puerto}`);
+});
+
+
