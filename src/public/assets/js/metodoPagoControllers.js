1ec18a2f2f7de93850aaefa98ad2c824d5da8647
Luis Guillen <luisguillenortiz_8@hotmail.com>
2023-08-13 11:50:00 -0600

    renderizando metodo pago

---
+++ src/public/assets/js/metodoPagoControllers.js
@@ -0,0 +1,25 @@
+
+var metodos='';
+
+
+const obtenerMetodoPago = async () => {
+	const respuesta = await fetch("http://localhost:3001/metodoPago", {
+		method: "get",
+	});
+	metodosdePago = await respuesta.json();
+    console.log("Metodos de Pago", metodosdePago);
+    listaMetodosPago();
+};
+
+obtenerMetodoPago();
+
+
+function listaMetodosPago() {
+    document.getElementById('metodoActual').innerHTML='';
+    for (let i = 0; i < metodosdePago.length; i++) {
+        document.getElementById('metodoActual').innerHTML+=
+    ` <option value="${i+1}">${metodosdePago[i].nombre}</option>`
+    
+    }
+    
+}
